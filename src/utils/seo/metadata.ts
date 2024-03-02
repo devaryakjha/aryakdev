36ef597642ae46105d2fffd6c707ea5135cd9a47
logiclynxAJ <me@aryak.dev>
2024-03-03 04:12:46 +0530

    Add SEO metadata and og:image for portfolio website

--- src/utils/seo/metadata.ts
+++ src/utils/seo/metadata.ts
@@ -1,12 +1,32 @@
 import type { Metadata } from "next";
 
+const title = "Aryakumar Jha - Software Engineer Portfolio";
+const description =
+  "Welcome to the portfolio website of Aryakumar Jha, a skilled software engineer specializing in mobile and web application development. Explore Aryakumar's projects and expertise in Flutter, React.js, Node.js, and more.";
+const keywords =
+  "Software Engineer, Portfolio, Mobile App Development, Web Development, Flutter, React.js, Node.js, Golang, Python, UI/UX Design, Full-stack Development";
+const applicationName = title;
+const url =
+  process.env.VERCEL_ENV === "production"
+    ? "https://aryak.dev"
+    : process.env.VERCEL_ENV === "preview"
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
+
 const metadata: Metadata = {
-  title: "Aryakumar Jha - Software Engineer Portfolio",
-  description:
-    "Welcome to the portfolio website of Aryakumar Jha, a skilled software engineer specializing in mobile and web application development. Explore Aryakumar's projects and expertise in Flutter, React.js, Node.js, and more.",
-  keywords:
-    "Software Engineer, Portfolio, Mobile App Development, Web Development, Flutter, React.js, Node.js, Golang, Python, UI/UX Design, Full-stack Development",
-  applicationName: "Aryakumar Jha - Software Engineer Portfolio",
+  title,
+  description,
+  keywords,
+  applicationName,
+  openGraph: {
+    title,
+    description,
+    url,
+    siteName: applicationName,
+    images: {
+      url: `${url}/images/og-image.png`,
+    },
+  },
 };
 
 export default metadata;
