758a05675e4a4ed6a95167d2e9b3cdc24ab1af33
logiclynxAJ <me@aryak.dev>
2024-03-05 20:06:42 +0530

    Add styles for page module and navbar module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <section className={styles.intro}>
+        <Image
+          src="/images/map_light.svg"
+          width={500}
+          height={540}
+          alt="India map"
+          className={styles.map}
+        />
+      </section>
+    </main>
+  );
 }
