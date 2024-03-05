c05f0887dd9388f6afcd7c82e735a313fcd1d577
logiclynxAJ <me@aryak.dev>
2024-03-05 13:20:40 +0530

    Update BASE_URL in constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,5 +2,5 @@ export const BASE_URL =
   process.env.VERCEL_ENV === "production"
     ? "https://aryak.dev"
     : process.env.VERCEL_ENV === "preview"
-    ? `https://${process.env.VERCEL_URL}`
+    ? `https://dev.aryak.dev`
     : "https://localhost:3000";
