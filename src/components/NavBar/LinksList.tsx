86c0fa1d51f59b8b4c84c270a247134aa53d4e1e
logiclynxAJ <dev.jha.arya@gmail.com>
2024-03-20 21:54:55 +0530

    WIP

---
+++ src/components/NavBar/LinksList.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Button from "@/ui/Button/button";
+import styles from "./Navbar.module.css";
+import { usePathname } from "next/navigation";
+
+export default function LinksList() {
+  const pathname = usePathname();
+  const isRoot = pathname === "/";
+  if (!isRoot) {
+    return <></>;
+  }
+  return (
+    <>
+      <li className={styles.navitem} key={"/projects"}>
+        <a href="#projects">
+          <Button variant={{ variant: "navigation" }}>Projects</Button>
+        </a>
+      </li>
+      <li className={styles.navitem} key={"/blog"}>
+        <a href="/blog">
+          <Button variant={{ variant: "navigation" }}>Blog</Button>
+        </a>
+      </li>
+    </>
+  );
+}
