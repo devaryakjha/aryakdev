8e2dcf3be1684a3c80030cb3ee14908954a5ef0d
logiclynxAJ <dev.jha.arya@gmail.com>
2024-03-20 22:11:11 +0530

    Added config for middleware syayos asseys

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,25 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+  ],
+};
+
 export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
 
