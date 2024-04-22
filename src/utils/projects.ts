439177d96bf8272755fb55c8a60fdb7e7cc06f18
logiclynxAJ <dev.jha.arya@gmail.com>
2024-04-22 07:51:58 +0530

    Add Cipher Forge project and update Varanasi project details

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -10,15 +10,24 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 1,
+    title: "Cipher Forge",
+    description:
+      "Generate secure and strong passwords effortlessly with CipherForge. Our free online password generator tool ensures your accounts remain protected against cyber threats.",
+    image: "/images/projects/cipher_forge.webp",
+    github: "https://cipher-forge.aryak.dev",
+    color: "#666597",
+  },
+  {
+    id: 2,
     title: "Project Varanasi",
     description:
       "This is a work-in-progress music streaming application built with Flutter for iOS, Android and macOs (Beta). The app is designed to have all the basic functionalities of a music app, with a UI heavily inspired by the Spotify mobile app.",
     image: "/images/projects/varanasi.webp",
-    github: "https://github.com/logiclynxAJ/varanasi_mobile_app",
+    github: "https://github.com/devaryakjha/varanasi",
     color: "#C38D4F",
   },
   {
-    id: 2,
+    id: 3,
     title: "Dictionary",
     description:
       "This user-friendly app offers cross-platform compatibility, enhances language learning with pronunciation audios, supports multiple parts of speech understanding, and optimizes offline access through audio file caching. Empowering efficient language learning with convenience and accuracy.",
@@ -27,7 +36,7 @@ export const projects: Project[] = [
     color: "#999AC6",
   },
   {
-    id: 3,
+    id: 4,
     title: "BMI Calculator",
     description:
       "A Flutter app to calculate BMI visually. Users input height & weight, BMI is calculated using (weight / (height * height)). BMI value is displayed with a graphical representation of corresponding BMI range (underweight, normal weight, overweight, or obese). App also explains BMI & calculation methodology.",
